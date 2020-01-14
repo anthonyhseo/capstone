41b69ea5b03a2c7d9ef10f34ba752918a2f877a3
Anthony Seo <anthonyhseo@gmail.com>
2020-01-14 14:30:41 -0800

    started authentication

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,31 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const path = require('path')
 const cors = require('cors')
 const multer = require('multer')
 
+const users = require('./routes/users')
+const classify = require('./routes/classify')
+
 const imageClassification = require('./classify.js')
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' })
 
 const PORT = process.env.PORT || 3001
 
+connectDB()
 const app = express()
-
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-const storage = multer.diskStorage({
-  destination: './public/uploads',
-  filename: (req, file, cb) => {
-    cb(null, 'IMAGE-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10000000 }
-})
+app.use('/api/v1/users', users)
+app.use('/api/v1/classify', classify)
 
 app.get('/', (req, res) => {
   res.send('Got the request')
-})
-
-app.post('/api/upload', upload.single('myFile'), async (req, res, next) => {
-  const file = req.file
-  if (!file) {
-    const error = new Error('Please upload a file')
-    error.httpStatusCode = 400
-    return next(error)
-  }
-
-  const result = await imageClassification(`./public/uploads/${file.filename}`)
-  console.log(result)
-  res.send(result)
 })
 
 app.listen(PORT, () => {
