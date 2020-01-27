ecb050c22c2bc5a9fd56f54259a7298807da046f
@slarsen <stu@iointel.com>
2020-01-27 14:15:17 -0800

    newest change

--- client/src/components/Photo/Photo.js
+++ client/src/components/Photo/Photo.js
@@ -3,9 +3,10 @@ import './Photo.css'
 
 export default function Photo(props) {
     return (
-        <div className='photo'>
+        <div className='photo-card'>
             <p>{props.name}</p>
             <img src={`${props.image}.jpg`}/>
         </div>
     )
 }
+ 
