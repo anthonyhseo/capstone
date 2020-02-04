e35802cc5b1beca25e54515af918aeff66d2b72a
Simone Ray <44311795+sim1ray@users.noreply.github.com>
2020-02-03 21:02:51 -0800

    Update UploadPhoto.js

--- client/src/components/UploadPhoto/UploadPhoto.js
+++ client/src/components/UploadPhoto/UploadPhoto.js
@@ -50,7 +50,6 @@ class UploadPhoto extends Component {
     axios.defaults.headers['Authorization'] =
       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIzIiwiaWQiOiI1ZTI4ZTEwMDkwMmVmOTFhMjk0ZTBjNjIiLCJpYXQiOjE1ODA3MTI2ODcsImV4cCI6MTU4MDc5OTA4N30.F5iQT1um-LzJcmkx-QtNHOi8CFL6wOBcMhYjwU7_Dz4'
     axios
-      // .post('http://ec2-54-202-80-154.us-west-2.compute.amazonaws.com:3001/api/v1/classify', form)
       // .post('http://localhost:3001/api/v1/classify', form)
       .post('http://localhost:3001/api/v1/classify/authClassify', form)
       .then(response => {
