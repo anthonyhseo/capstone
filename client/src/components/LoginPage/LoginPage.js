94db9a7ae5a16bde7735d5e8453826a40a992bd4
Anthony Seo <anthonyhseo@gmail.com>
2020-02-12 16:48:24 -0800

    login page checkpoint

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { loginUser } from '../../actions/authActions'
-import axios from 'axios';
+import axios from 'axios'
 import './LoginPage.css'
 
 class LoginPage extends Component {
-  
+  constructor(props) {
+    super(props)
 
-    state = {
+    this.state = {
       username: '',
       password: ''
     }
+  }
 
-    
-    handleChange(e) {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
   onClick(e) {
     this.props.loginUser()
@@ -30,7 +31,7 @@ class LoginPage extends Component {
     console.log(this.state)
 
     // Destructure state
-    const {username, password} = this.state
+    const { username, password } = this.state
 
     // Create a user object to pass as the body in POST request
     const user = {
@@ -38,18 +39,21 @@ class LoginPage extends Component {
       password
     }
 
-
     try {
       const config = {
         headers: {
           'Content-Type': 'application/json'
         }
-      }  
+      }
 
       const body = JSON.stringify(user)
-      const res = await axios.post('http://ec2-54-202-80-154.us-west-2.compute.amazonaws.com:3001/api/v1/users/login', body, config)
+      const res = await axios.post(
+        'http://localhost:3001/api/v1/users/login',
+        body,
+        config
+      )
+      localStorage.setItem('jwtToken', res)
       console.log(res.data)
-
     } catch (err) {
       console.error(err.response.data)
     }
@@ -57,34 +61,31 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div className='login-container'>
-        <div className='login'>
+      <div className="login-container">
+        <div className="login">
           <h2>Please Login</h2>
           <form onSubmit={e => this.onSubmit(e)}>
             <input
               placeholder="Username"
-              type='text'
-              name='username'
+              type="text"
+              name="username"
               value={this.state.username}
               onChange={e => this.handleChange(e)}
               required
             />
             <input
               placeholder="Password"
-              type='password'
-              name='password'
+              type="password"
+              name="password"
               value={this.state.password}
               onChange={e => this.handleChange(e)}
               required
             />
-          
-          <button type='submit'>
-            Login
 
-          </button>
+            <button type="submit">Login</button>
           </form>
           {/* The Link is used to go straight to the dashboard */}
-        {/* <Link onClick={e => this.onClick(e)} to="/dashboard" className="btn btn-primary login-btn">
+          {/* <Link onClick={e => this.onClick(e)} to="/dashboard" className="btn btn-primary login-btn">
           Login
         </Link> */}
         </div>
@@ -93,7 +94,4 @@ class LoginPage extends Component {
   }
 }
 
-export default connect(
-  null,
-  { loginUser }
-)(LoginPage)
+export default connect(null, { loginUser })(LoginPage)
